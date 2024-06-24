b7793e414b78c561410c08fd2190fa1b5ef96429
Bhavana-10 <140387889+Bhavana-10@users.noreply.github.com>
2024-06-24 17:27:47 +0530

    Add files via upload

--- Diffca.java
+++ Diffca.java
@@ -1,7 +1,6 @@
 package collections;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 
 public class Diffca {
@@ -9,12 +8,12 @@ public class Diffca {
 	public static void main(String[] args) 
 	{
 	//Array
- /*   String[] a=new String[] {"w","e","l","c","o","m","e"};
+    String[] a=new String[] {"w","e","l","c","o","m","e"};
     for(int i=0;i<a.length;i++)
     {
     	System.out.print(a[i]);
     }
-    System.out.println();*/
+    System.out.println();
     //collections
     ArrayList<String> al=new ArrayList<>();
     al.add("w");
@@ -24,25 +23,13 @@ public class Diffca {
     al.add("o");
     al.add("m");
     al.add("e");
-    al.add(0,"hi ");
-  //  System.out.println(al);
- //   System.out.println(al.get(4));
-  /*  al.set(1,"to Java");
-    System.out.print(al);*/
- /*  System.out.println(al.indexOf("e"));
-   System.out.println(al.lastIndexOf("e"));*/
-   // System.out.println(al);
-  /*  al.remove(0);
-    System.out.println(al);*/
-  //  System.out.println(al.subList(4, 8));
-   /* Iterator<String> itr=al.iterator();
+    System.out.println(al);
+    
+    Iterator<String> itr=al.iterator();
     while(itr.hasNext())
     {
     	System.out.print(itr.next());
-    }*/
-    //Collections.sort(al);
-   // Collections.reverse(al);
-    Collections.sort(al,Collections.reverseOrder());
-    System.out.println(al);
+    }
 	}
+
 }
